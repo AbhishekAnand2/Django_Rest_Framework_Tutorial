54a3ed8d82b12edda54879065841c93ae13481a9
Abhi <as1965286@gmail.com>
2022-10-12 01:25:28 +0530

    Commit_03

--- workspace/py_client/basic.py
+++ workspace/py_client/basic.py
@@ -2,4 +2,17 @@ import requests
 
 
 # Endpoints are the places where we have to made a request to.
-endpoint = ""
+# endpoint = "https://httpbin.org/"
+endpoint = "http://localhost:9000/api/" # or http://127.0.0.1:9000/
+
+get_response = requests.get(endpoint, json={"query": "Hello World!"}) #HTTP Request
+print(get_response.text) # Print raw text response
+print(get_response.status_code) # Status code returned
+print(get_response.json())
+
+
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Notation -> Python Dict
+# print(get_response.json())
+# print(get_response.status_code) 
