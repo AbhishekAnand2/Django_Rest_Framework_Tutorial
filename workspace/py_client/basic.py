5e863a04f8359c68fe5d56a425769c0eff859854
as1965286 <as1965286@gmail.com>
2022-10-26 01:19:20 +0530

    change1

--- workspace/py_client/basic.py
+++ workspace/py_client/basic.py
@@ -5,14 +5,14 @@ import requests
 # endpoint = "https://httpbin.org/"
 endpoint = "http://localhost:9000/api/" # or http://127.0.0.1:9000/
 
-get_response = requests.get(endpoint, json={"query": "Hello World!"}) #HTTP Request
+get_response = requests.get(endpoint, params={"abc":123}, json={"query": "Hello World!"}) #HTTP Request
 print(get_response.text) # Print raw text response
 print(get_response.status_code) # Status code returned
 print(get_response.json())
 
 
-# HTTP Request -> HTML
-# REST API HTTP Request -> JSON
+# HTTP Request -> HTML (These are the requests that humans made using browser)
+# REST API HTTP Request -> JSON (These are the Http Request which an API can make)
 # JavaScript Object Notation -> Python Dict
 # print(get_response.json())
 # print(get_response.status_code) 
